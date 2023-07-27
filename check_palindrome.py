a39d860c2e0196db27ba3e75f7aa8476337fccb7
Brian-M-J <brianmj02@gmail.com>
2023-07-27 22:50:37 +0530

    Add 6 lab programs
    Add programs 1-a, 1-b, 2-a, 2-b, 3-a and 3-b to the repo.

---
+++ check_palindrome.py
@@ -0,0 +1,9 @@
+# Palindrome
+str_val: str = input("Enter a value: ")
+if str_val == reversed(str_val):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+for digit in sorted(set(str_val)):
+    print(f"{digit} appears {str_val.count(digit)} times")
---
+++ sentence_statistics.py
@@ -0,0 +1,17 @@
+# Sentence Statistics
+sentence: str = input("Enter a sentence: ")
+word_list: list[str] = sentence.split()
+print(f"This sentence has {len(word_list)} words.")
+dig_cnt = lo_cnt = up_cnt = 0
+
+for ch in sentence:
+    if ch.isdigit():
+        dig_cnt += 1
+    elif ch.islower():
+        lo_cnt += 1
+    elif ch.isupper():
+        up_cnt += 1
+
+print(
+    f"This sentence has {dig_cnt} digits, {up_cnt} upper case letters, {lo_cnt} lower case letters."
+)
